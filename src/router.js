2c21898994f131d9bc64559de9a1a52c9332b8d7
zurda <m.weizman@gmail.com>
2018-11-21 16:37:17 +0000

    Filtered URL endpoints to searver

--- src/router.js
+++ src/router.js
@@ -2,8 +2,18 @@ const fs = require('fs');
 const path = require('path');
 // const port = 5000;
 
+const extensionType = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  ico: 'image/x-ico',
+  jpg: 'image/jpeg',
+  png: 'image/png'
+};
+
 const router = (request, response) => {
   const url = request.url;
+  const extension = url.split('.')[1];
 
   if(url === '/') {
     const filePath = path.join(__dirname, '..', 'public', 'index.html');
@@ -18,7 +28,25 @@ const router = (request, response) => {
         response.end(file);
       }
     });
+  } else if (url !== '/') {
+    // handeling generic files
+    const filePath = path.join(__dirname, '..', 'public', url);
+    fs.readFile(filePath, (error, file) => {
+      if(error) {
+        console.log(error)
+        response.writeHead(500, {'Content-Type':'text/html'})
+        response.end('this is an error');
+        return;
+      } else {
+        response.writeHead(200, {'Content-Type':`${extensionType[extension]}`});
+        response.end(file);
+      }
+    });
   }
+  //else {
+    // handeling errors
+  //}
+
 }
 
 
