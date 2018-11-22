355ff06e84007ca8412033b82fcfa75e8a7856c5
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-11-22 17:07:25 +0000

    basic css

--- public/index.js
+++ public/index.js
@@ -1,23 +1,25 @@
 // output JSON and do DOM stuff here
-function() {
-  const handleError = error => {
-    const h2 = document.createElement("h2");
-    h2.textContent = "Server HandleError";
-    document.querySelector("#error").appendChild(h2);
-  };
+(function() {
+    const handleError = error => {
+        const h2 = document.createElement("h2");
+        h2.textContent = "Server HandleError";
+        document.querySelector("#error").appendChild(h2);
+    };
 
-  const renderData = dataString => {
-    console.log("Render Data console log: " + dataString);
-    const h2 = document.createElement("h2");
-    h2.textContent = dataString;
-    document.querySelector("#error").appendChild(h2);
-  };
+    const renderData = dataString => {
+        console.log("Render Data console log: " + dataString);
+        const h2 = document.createElement("h2");
+        h2.textContent = dataString;
+        document.querySelector("#error").appendChild(h2);
+    };
 
-  request.get("/src/data/sample.json", function(error, response) {
-    if (error) {
-      handleError(error);
-    } else {
-      renderData(response);
-    }
-  });
-};
+
+
+    request.get("/src/data/sample.json", function(error, response) {
+        if (error) {
+            handleError(error);
+        } else {
+            renderData(response);
+        }
+    });
+})();
